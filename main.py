50a79cd2a653fc2017db26eb721734fe0858ea98
Meirbek Beisenov <meirbek1evo@gmail.com>
2021-11-06 22:53:52 +0600

    commit

--- main.py
+++ main.py
@@ -2,19 +2,18 @@ import sys
 # noinspection PyCompatibility
 from urllib.request import urlopen  # Для загрузки содержимого страницы с заданного URL
 
-import PyQt6.QtWidgets
+from PyQt6 import QtWidgets
 from PyQt6.QtCore import Qt
 from PyQt6.uic import loadUi
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup  # Для навигации по HTML-разметке страницы
 
 
-# noinspection PyCompatibility
-
-class Main(PyQt6.QtWidgets.QDialog):
+class Main(QtWidgets.QDialog):
     def __init__(self):
         super(Main, self).__init__()
-        loadUi('scraper_form.ui', self)
+        loadUi('scraper_form.ui', self)  # Загрузка пользовательского интерфейса
 
+        # Привязка кнопок к методам при нажатии
         self.pushButton_scrap.clicked.connect(self.scrap)
         self.pushButton_clear.clicked.connect(self.clear)
         self.pushButton_exit.clicked.connect(self.exit)
@@ -22,17 +21,17 @@ class Main(PyQt6.QtWidgets.QDialog):
     def scrap(self):
         try:
             self.label_status.setText('Данные загружаются')
-            PyQt6.QtWidgets.QApplication.setOverrideCursor(Qt.CursorShape.WaitCursor)
+            QtWidgets.QApplication.setOverrideCursor(Qt.CursorShape.WaitCursor)  # Изменение формы курсора
             self.label_status.setStyleSheet('color: rgb(100, 100, 100); font: bold')
             username = str(self.lineEdit_username.text())
             url = "https://github.com/" + username
             sauce = urlopen(url).read()
-            soup = bs(sauce, 'lxml')  # lxml это парсер
+            soup = BeautifulSoup(sauce, 'lxml')  # lxml это парсер
             repo_amount = int(soup.find('span', class_='Counter').text)
             self.textEdit_info.setText("Количество репозиториев: " + str(repo_amount))
             url2 = url + "?tab=repositories"
             sauce = urlopen(url2).read()
-            soup = bs(sauce, 'lxml')
+            soup = BeautifulSoup(sauce, 'lxml')
             arr = [0]
             tags = soup.find_all('a', itemprop="name codeRepository")
             for tag in tags:
@@ -43,7 +42,7 @@ class Main(PyQt6.QtWidgets.QDialog):
                 url3 = url + "?page=" + str(k) + "&tab=repositories"
                 k += 1
                 sauce = urlopen(url3).read()
-                soup = bs(sauce, 'lxml')
+                soup = BeautifulSoup(sauce, 'lxml')
                 tags = soup.find_all('a', itemprop="name codeRepository")
                 for tag in tags:
                     if tag.text != "":
@@ -53,9 +52,9 @@ class Main(PyQt6.QtWidgets.QDialog):
                 self.textEdit_info.append(h1)
             self.label_status.setText('Данные загружены')
             self.label_status.setStyleSheet('color: rgb(0, 200, 0); font: bold')
-            PyQt6.QtWidgets.QApplication.restoreOverrideCursor()
+            QtWidgets.QApplication.restoreOverrideCursor()
         except Exception:
-            PyQt6.QtWidgets.QApplication.restoreOverrideCursor()
+            QtWidgets.QApplication.restoreOverrideCursor()
             self.label_status.setText("Введено неверное имя пользователя")
             self.textEdit_info.setText('')
             self.label_status.setStyleSheet('color: rgb(200, 0, 0); font: bold')
@@ -70,7 +69,7 @@ class Main(PyQt6.QtWidgets.QDialog):
 
 
 if __name__ == '__main__':
-    app = PyQt6.QtWidgets.QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     window = Main()
     window.show()
     sys.exit(app.exec())
